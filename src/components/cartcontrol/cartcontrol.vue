e4122a6c069e48ed8cf8f2ff035e4da1e5fd3b9e
ruoxuexi <17687396540@163.com>
2019-06-19 22:04:31 +0800

    再次理解动画

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
-      <i class="el-icon-remove-outline"></i>
-    </div>
+    <transition name="decrease">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <i class="el-icon-remove-outline"></i>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add"  @click="addCart">
       <i class="el-icon-circle-plus"></i>
@@ -39,6 +41,15 @@
   .cartcontrol{
     font-size: 0;
     .cart-decrease{
+      &.decrease-enter-active, &.decrease-leave-active{
+        transition: all .4s linear;
+        opacity: 1;
+        transform: translate3D(0, 0, 0);
+      }
+      &.decrease-enter, &.decrease-leave-to{
+        opacity: 0;
+        transform: translate3D(24px, 0, 0);
+      }
       display: inline-block;
       line-height: 24px;
       .el-icon-remove-outline{
